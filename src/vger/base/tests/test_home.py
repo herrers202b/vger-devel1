fdf5bc3f6fe19afb7895b722971e2b0b7110f8c8
goldencm <goldencm203@potsdam.edu>
2021-03-11 12:21:58 -0500

    added test for Home

---
+++ src/vger/base/tests/test_home.py
@@ -0,0 +1,17 @@
+from django.test import RequestFactory, TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+try:
+    from ..views import home
+except ImportError:
+    print("The home view is not implemented for testing import!")
+
+
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_home_url_is_resolved(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, home)
