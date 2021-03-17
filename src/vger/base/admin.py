c88823e0e8bcf0efcb4d136f6014e2a164aa9216
scaccimj202@potsdam.edu <scaccimj202@potsdam.edu>
2021-03-17 11:13:36 -0400

    Corrected database dependencies such that questions, categories, and surveys are linked and deleted properly. Connected said models to admin.py, documented code.

--- src/vger/base/admin.py
+++ src/vger/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Question, Category, Survey
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(Survey)
--- src/vger/base/models.py
+++ src/vger/base/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from user import models
 # Create your models here.
 
 #Untested question model
@@ -19,32 +18,25 @@ class Question(models.Model):
     QUESTION_WEIGHTS : choice
         QUESTION_WEIGHTS will be a choice/radio button that allows the user
         to select from 0 to 4 on a scale of weights for some questionText
-    
-    weights[0-4] : str
-        These strings can be either left blank or filled in with specific 
-        prompts to the user. This is done to let the admin make a more
-        personalized survey question for a given category
-
+    category : forign key
+        category is our forign key to the category model.
+         Used to link questions to categories
     """
-    weight0 = models.CharField("Statement or question", max_length=50)
-    weight1 = models.CharField("Statement or question", max_length=50)
-    weight2 = models.CharField("Statement or question", max_length=50)
-    weight3 = models.CharField("Statement or question", max_length=50)
-    weight4 = models.CharField("Statement or question", max_length=50)
     #Choices bank with weights and questions
     QUESTION_WEIGHTS = (
-        ('0',weight0),
-        ('1',weight1),
-        ('2',weight2),
-        ('3',weight3),
-        ('4',weight4),
+        (0,'weight 0'),
+        (1,'weight 1'),
+        (2,'weight 2'),
+        (3,'weight 3'),
+        (4,'weight 4'),
     )
-    #Text of question or statement being presented
     questionText = models.CharField(max_length=100, help_text="Please enter a prompt. ex) I know how to install software on my computer.")
-    #Score attained from radio buttons
-    score = models.IntegerField(choices=QUESTION_WEIGHTS, blank=True, default='2', help_text="Results of question")
+    score = models.IntegerField(choices=QUESTION_WEIGHTS, blank=True, null=True, help_text="Results of question")
+    category = models.ForeignKey("Category", verbose_name=("Parent Category"), default=None, null=True, on_delete=models.CASCADE)
+
+    """String for representing the Question object."""
     def __str__(self):
-        return f'({self.questionText})'
+        return f'{self.questionText}'
     
 
 #Untested character model
@@ -62,16 +54,18 @@ class Category(models.Model):
         lowWeightText flavor text for our lower weight header, ex "Not like me at all"
     highWeightText : str
         highWeightText flavor text for our high weight header, ex "Extremely like me"
-    question : forign key
-        question is our forign key to the question model. Used to link questions to categories
+    survey : forign key
+        survey will be our forign key to the survey model such 
+        that we can link categoies of surveys.
     """
     titleOfCategory = models.CharField(max_length=100, help_text="Please enter a title for this category, ex) Computer Skills.")
-    lowWeightText = models.CharField(max_length=50, help_text="Please enter flavor text for the low weight of the category, ex) Not like me at all")
-    highWeightText = models.CharField(max_length=50, help_text="Please enter flavor text for the high weight of the category, ex) Extremely like me")
-    question = models.ForeignKey("Question", verbose_name=("Question"), on_delete=models.CASCADE)
+    lowWeightText = models.CharField(max_length=50, default="Not like me at all", help_text="Please enter flavor text for the low weight of the category, ex) Not like me at all")
+    highWeightText = models.CharField(max_length=50, default="Extremely like me", help_text="Please enter flavor text for the high weight of the category, ex) Extremely like me")
+    survey = models.ForeignKey("Survey", verbose_name=("Parent Survey"), default=None, null=True, on_delete=models.CASCADE)
 
+    """String for representing the Category object."""
     def __str__(self):
-        return f'({self.titleOfCategory})'
+        return f'{self.titleOfCategory}'
     
 #Untested survey model
 class Survey(models.Model):
@@ -86,20 +80,11 @@ class Survey(models.Model):
         titleOfSurvey serves as the title of our survey, ex "VGER"
     directions : str
         directions will allow an admin to write any specific
-        directions for their survey
-    category : forign key
-        category will be our forign key to the category model such 
-        that we can link categoies of questions to our survey. 
-
-    Returns
-    -------
-    int
-        Description of return value
-
+        directions for their survey 
     """
     titleOfSurvey = models.CharField(max_length=50, help_text="Please enter a name for the survey")
     directions = models.CharField(max_length=500, help_text="Please enter any directions to take the survey")
-    category = models.ForeignKey("Category", verbose_name=("Category"), default=None, on_delete=models.CASCADE)
     
+    """String for representing the Survey object."""
     def __str__(self):
-        return f'({self.titleOfSurvey})'
+        return f'{self.titleOfSurvey}'
