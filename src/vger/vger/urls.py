fb66e7bb9109e9aff9e2339a522c35fb6e5a56f2
Saira Herrera <sairaherrera@192.168.1.6>
2021-03-18 19:38:43 -0400

    added login

--- src/vger/vger/urls.py
+++ src/vger/vger/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.contrib.auth import views as auth_views
 
 from base import urls
 from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('survey/', views.SurveyListView.as_view(), name='survey'),
-    path('survey/<int:pk>', views.SurveyDetailView.as_view(), name='survey-detail'),
+    path('', include('base.urls')),
+    path('', include('user.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
