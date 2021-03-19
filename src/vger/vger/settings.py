95275585e4fc00b3a431a3d0f5d819ea6315a374
Saira Herrera <sairaherrera@192.168.1.6>
2021-03-18 20:31:44 -0400

    added logout

--- src/vger/vger/settings.py
+++ src/vger/vger/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'base.apps.BaseConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/vger/vger/urls.py
+++ src/vger/vger/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', include('base.urls')),
     path('', include('user.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
