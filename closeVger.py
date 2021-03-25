cfd6c6f06f5bcbcd05cb8685cef2ca9b931fecc6
butlertj10 <butlertrevor36@gmail.com>
2021-03-25 18:54:34 -0400

    Allows for script to be ran based off the OS you are using. MacOS and Windows Powershell need tweaking. Verified that Linux and Powershell will work, MacOS is unknown.

--- closeVger.py
+++ closeVger.py
@@ -1,6 +1,6 @@
 # The closeVger.py script will close down the website 
 # # 
-# To execute this script type in the command line: python3 ./closeVger
+# To execute this script type in the command line: python3 ./closeVger.py
 # Note: Must be in the /vger-devel1-dream-team directory to execute
 
 import os
