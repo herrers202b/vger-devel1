e50ee70fdf1ecf4c317b52d9817bc45320b26399
Saira <49323735+herrers202b@users.noreply.github.com>
2021-04-13 22:38:26 -0400

    fixed up Windows

--- run_Initial_Install.py
+++ run_Initial_Install.py
@@ -4,13 +4,18 @@ import sys
 # Builds the Docker images,
 # creates necessary directories with appropriate ownership,
 # spins up the system to build empty databases,
-# and spin the system back down. 
+# and spin the system back down.
 
 # Mac, Linux
 # 1. Build the server
 # 2. Set up a root user
 # 3. Spin the server up
 # 4. Spin the server down
+
+# Windows
+# 1. Build the server
+# 2. Spin the server up
+# 3. Spin the server down
 
 def linuxInstall():
     print("*************** Build the Server ***************")
@@ -40,6 +45,17 @@ def macInstall():
     os.system('python3 run.py down')
     print("Initial Install was ran on your MacOS and your site is back down.")
 
+def windowsInstall():
+    print("*************** Build the Server ***************")
+    os.system('docker-compose build')
+
+    print("*************** Spin up the Server ***************")
+    os.system('docker-compose up')
+
+    print("*************** Spin the Server Down ***************")
+    os.system('docker-compose down')
+    print("Initial Install was ran on your Windows and your site is back down.")
+
 def get_platform():
     platforms = {
         'linux1': 'Linux',
@@ -55,6 +71,8 @@ def get_platform():
 def main():
     if get_platform() == "linux":
         linuxInstall()
+    elif get_platform() == "Windows":
+        windowsInstall()
     elif get_platform() == "OS X":
         macInstall()
     else:
